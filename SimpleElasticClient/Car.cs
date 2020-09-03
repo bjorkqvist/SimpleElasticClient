5cdb23adbb49b659eba0d1162b50321d6f8b7d1d
Anton Björkqvist <anton.bjorkqvist@svenskaspel.se>
2020-09-03 10:44:49 +0200

    Add project files.

---
+++ SimpleElasticClient/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleElasticClient
+{
+    internal class Car
+    {
+        public string brand { get; }
+        public int miles { get; }
+
+    public Car(string v1, int v2)
+        {
+            this.brand = v1;
+            this.miles = v2;
+        }
+    }
+}
---
+++ SimpleElasticClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Elasticsearch.Net;
+using System;
+using System.Threading.Tasks;
+
+namespace SimpleElasticClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var settings = new ConnectionConfiguration()
+                .PrettyJson();
+
+            var client = new ElasticLowLevelClient(settings);
+
+            var car = new Car("Volvo", 20001);
+
+            var response = await client.IndexAsync<StringResponse>("car", PostData.Serializable(car));
+            var responseString = response.Body;
+
+            
+
+            Console.WriteLine(responseString);
+
+        }
+    }
+}
